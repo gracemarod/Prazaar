55794a9feead3254756a748e4b4555b7e4350949
asvnpr <vega360@gmail.com>
2016-05-22 13:49:40 -0400

    testing if scaffold is working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522083626) do
+ActiveRecord::Schema.define(version: 20160522165303) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "username"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160522083626) do
   end
 
   add_index "artists", ["email"], name: "index_artists_on_email", unique: true
+
+  create_table "artworks", force: :cascade do |t|
+    t.text     "description"
+    t.float    "price"
+    t.integer  "stock"
+    t.string   "photos"
+    t.string   "category"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
